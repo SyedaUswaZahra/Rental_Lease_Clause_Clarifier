84cf81a798aa98bb4f1d3c8232d2831887d47093
Uswa <editedasuswa22@gmail.com>
2026-08-05 13:35:55 +0500

    Add loaders/__init__.py via API

---
+++ loaders/__init__.py
@@ -0,0 +1,5 @@
+from loaders.resume_loader import ResumeLoader
+from loaders.job_description_loader import JobDescriptionLoader
+from loaders.text_splitter import TextSplitter
+
+__all__ = ["ResumeLoader", "JobDescriptionLoader", "TextSplitter"]
