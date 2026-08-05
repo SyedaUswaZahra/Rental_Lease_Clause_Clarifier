baf2538aa0bb2cfdb9f1b39727dd5dfc57572454
Uswa <editedasuswa22@gmail.com>
2026-08-05 13:35:40 +0500

    Add chains/__init__.py via API

---
+++ chains/__init__.py
@@ -0,0 +1,11 @@
+from chains.keyword_extraction import KeywordExtractionChain
+from chains.bullet_rewrite import BulletRewriteChain
+from chains.cover_letter import CoverLetterChain
+from chains.gap_analysis import GapAnalysisChain
+
+__all__ = [
+    "KeywordExtractionChain",
+    "BulletRewriteChain",
+    "CoverLetterChain",
+    "GapAnalysisChain",
+]
