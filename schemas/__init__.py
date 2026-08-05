c551490357fdfd3b6f606d5c136775d38efc51d7
Uswa <editedasuswa22@gmail.com>
2026-08-05 13:36:17 +0500

    Add schemas/__init__.py via API

---
+++ schemas/__init__.py
@@ -0,0 +1,13 @@
+from schemas.requirements_schema import JobRequirement, JobRequirementsList
+from schemas.resume_schema import ResumeSection, Resume, RewrittenBullet
+from schemas.gap_analysis_schema import GapAnalysisItem, GapAnalysisResult
+
+__all__ = [
+    "JobRequirement",
+    "JobRequirementsList",
+    "ResumeSection",
+    "Resume",
+    "RewrittenBullet",
+    "GapAnalysisItem",
+    "GapAnalysisResult",
+]
